f394f43f4654aa6723c6adaae27c272669730b5a
unknown <2400030224@kluniversity.in>
2025-11-30 06:12:53 +0530

    Setup for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/professional/",
   plugins: [react()],
-  base: '/',
-  build: { target: 'es2019' },
-  server: {
-    proxy: {
-      '/api': 'http://localhost:4000'
-    }
-  }
-});
+})
